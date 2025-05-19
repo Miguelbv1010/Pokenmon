1f9a1c161dce3e04577832aef6980a108f76eeed
Miguelbv1010 <miguelbv880@gmail.com>
2025-05-19 20:31:27 +0000

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useState,useEffect } from 'react'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { AppProvider } from './contexto/contexto';
 import './App.css'
+import {supabase} from "./supabase"
 import  Aleatorios from './Componentes/Aleatorios'
 import Capturas from './Componentes/Capturas'
 import Favoritos from './Componentes/Favoritos'
@@ -9,27 +10,67 @@ import Lista from './Componentes/Lista'
 import Pokemon from './Componentes/Pokemon'
 import Usuarios from './Componentes/Usuarios'
 import Menu from './Componentes/Menu';
+import Login from './Componentes/Login';
+import Registro from './Componentes/Registro';
+import Administrador from './Componentes/Administrador';
 
 
 function App() {
+const [usuario, setUsuario] = useState(null);
+const [cargando, setCargando] = useState(true);
 
-  return (
-     <AppProvider>
-    <Router>
-      <Menu/>
-      <Routes>
-      <Route path="/" element={<Lista />} />
-        <Route path="/usuarios" element={<Usuarios />} />
-        <Route path="/aleatorios" element={<Aleatorios />} />
-        <Route path="/capturas" element={<Capturas />} />
-        <Route path="/favoritos" element={<Favoritos />} />
-        <Route path="/Pokemon/:name" element={<Pokemon />} />
-      </Routes>
-    </Router>
-    </AppProvider>
-
-    )
+useEffect(() => {
+async function verificarSesion() {
+const { data: { session } } = await supabase.auth.getSession();
+setUsuario(session?.user || null);
+setCargando(false);
 }
+verificarSesion();
 
-export default App
-        
+// Escucha cambios en la sesión
+supabase.auth.onAuthStateChange((_event, session) => {
+
+setUsuario(session?.user || null);
+});
+}, []);
+
+if (cargando) return <p>Cargando...</p>;
+return (
+<AppProvider>
+<Router>
+{usuario && <Menu />}
+
+<Routes>
+<Route path="/" element={usuario ? <Lista /> : <Navigate to="/login"
+
+/>} />
+
+<Route path="/usuarios" element={usuario ? <Usuarios /> : <Navigate
+
+to="/login" />} />
+
+<Route path="/aleatorios" element={usuario ? <Aleatorios /> :
+
+<Navigate to="/login" />} />
+
+<Route path="/capturas" element={usuario ? <Capturas /> :
+
+<Navigate to="/login" />} />
+
+<Route path="/favoritos" element={usuario ? <Favoritos /> :
+
+<Navigate to="/login" />} />
+
+<Route path="/Pokemon/:name" element={usuario ? <Pokemon/> :
+
+<Navigate to="/login" />} />
+
+<Route path="/login" element={<Login/>} />
+<Route path="/Registro" element={<Registro/>} />
+<Route path="/Administrador" element={<Administrador/>} />
+</Routes>
+</Router>
+</AppProvider>
+);
+}
+export default App;
